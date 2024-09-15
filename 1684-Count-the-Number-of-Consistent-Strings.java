9b384fd67c4bb9e905627d47538cf36cab5bfcc9
Shikhar <shikharsingh22803@gmail.com>
2024-09-15 17:19:59 +0530

    [1684] [Time Beats: 89.42%] [Memory Beats: 98.36%] - LeetPush

---
+++ 1684-Count-the-Number-of-Consistent-Strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int mask=0;
+        for( char ch :allowed.toCharArray())
+        {
+            mask |=(1 <<(ch - 'a'));
+        }
+        int count =0;
+        for(String word:words)
+        {
+            boolean allCharPresent = true;
+            for(char ch: word.toCharArray())
+            {
+                if(((mask >> ( ch -'a')) & 1) ==0)
+                {
+                    allCharPresent = false;
+                    break;
+                }
+            }
+            if(allCharPresent)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
