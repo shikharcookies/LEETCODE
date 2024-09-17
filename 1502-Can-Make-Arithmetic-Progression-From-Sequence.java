de256d80c3a96feee9404557f3c64f74819fb01a
Shikhar <shikharsingh22803@gmail.com>
2024-09-17 19:02:01 +0530

    [1502] [Time Beats: 97.86%] [Memory Beats: 42.34%] - LeetPush

--- 1502-Can-Make-Arithmetic-Progression-From-Sequence.java
+++ 1502-Can-Make-Arithmetic-Progression-From-Sequence.java
@@ -1,19 +1,20 @@
-class Solution {
-    public boolean canMakeArithmeticProgression(int[] arr) {
-        List<Integer> ap = new ArrayList<>();
+import java.util.Arrays;  
 
-        for(int num : arr)
-        {
-            ap.add(num);
-        }
-        Collections.sort(ap);
-        for(int i=1;i<ap.size()-1;i++)
-        {
-            if(ap.get(i)-ap.get(i-1) != ap.get(i+1)-ap.get(i))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+class Solution {  
+    public boolean canMakeArithmeticProgression(int[] arr) {  
+        // Sort the array  
+        Arrays.sort(arr);  
+
+        // Calculate the common difference  
+        int commonDifference = arr[1] - arr[0];  
+
+        // Check if the difference is consistent  
+        for (int i = 1; i < arr.length - 1; i++) {  
+            if (arr[i + 1] - arr[i] != commonDifference) {  
+                return false; // If the difference changes, return false  
+            }  
+        }  
+
+        return true; // If we made it here, it is an arithmetic progression  
+    }  
 }
