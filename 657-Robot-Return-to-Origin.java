c18f6d66b5d9449d57baf0ef076813471695a403
Shikhar <shikharsingh22803@gmail.com>
2024-09-18 11:00:22 +0530

    [657] [Time Beats: 98.06%] [Memory Beats: 33.99%] - LeetPush

---
+++ 657-Robot-Return-to-Origin.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        for(char ch:moves.toCharArray())
+        {
+            if(ch=='R')
+            {
+                x++;
+            }
+            else if(ch=='L')
+            {
+                x--;
+            }
+            else if(ch=='U')
+            {
+                y++;
+            }
+            else
+            {
+                y--;
+            }
+        }
+         return x==0 && y==0;       
+    }
+}
