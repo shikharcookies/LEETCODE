a206e77bacb55961502c44acbaecd53ac109486e
Shikhar <shikharsingh22803@gmail.com>
2024-10-24 23:53:35 +0530

    [951] [Time Beats: 100.00%] [Memory Beats: 71.34%] - LeetPush

---
+++ DCP-10-24/951-Flip-Equivalent-Binary-Trees.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode x, TreeNode y) {
+          if(x==null && y==null) return true;
+        if(x==null || y==null) return false;
+        if(x.val != y.val) return false;
+
+        if(flipEquiv(x.left,y.left) && flipEquiv(x.right,y.right)) return true;
+        if(flipEquiv(x.left,y.right) && flipEquiv(x.right,y.left)) return true;
+        
+        return false;
+        
+    }
+}
